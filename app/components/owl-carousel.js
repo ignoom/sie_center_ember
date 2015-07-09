a0f5d78d8d30ae381dcffda9367bb4750be78b15
Oswaldo Avalos <oavalos@agari.com>
2015-07-09 11:22:39 -0700

    Owl-Carousel working

--- app/components/owl-carousel.js
+++ app/components/owl-carousel.js
@@ -4,7 +4,13 @@ export default Ember.Component.extend({
 	
 	_initializeCarousel: function(){
 		//console.log("Carrousel Initialized");
-		this.$().owlCarousel();
+		this.$().owlCarousel({
+			items: 1,
+			singleItem: true,
+			autoPlay: true,
+			autoHeight: true,
+			stopOnHover: true
+		});
 	}.on('didInsertElement')
 
 });
